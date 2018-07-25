ab33a5415cf42c26cc1ac201d8520bad800e450b
tussbaum <tussbaum@gmail.com>
2018-07-25 21:12:00 +0300

    Update surf.h

--- surf.h
+++ surf.h
@@ -12,14 +12,19 @@ private:
 	string label;
 	float * OG_points; // the original points that we got as an input
 
-
+	
 	Surf(float ** inputPoints, vector<Face> vecFaces, vector<Point> vecPoints, float quan[], string label);
-	void runVorn(float ** inputPoints);
+	//Tomer: TODO: add a copy c~tor
+	
+	void runVorn(float ** inputPoints); //Tomer: why do we need inputPoints?
 	void clearFaces(bool mask[]);
 public:
 	Surf* create(float ** inputPoints, bool mask[], float quan[], string label);
-	void smoothSurf(); //smooth the surf
+	void smooth(); //smooth the surf
+	void reduce(); //Tomer: to think about it, I think using exportReducedObj is not clear use
 	void exportObj(string output);
-	void exportRedusedObj(string output, int repeat); //export obj after reduse, repeat is the amount of times reduse will be ran(1 is usally the best)
+	void exportReducedObj(string output, int repeat); //export obj after reduce, repeat is the amount of times reduce will be ran(1 is usally the best)
+	
+	// Tomer: Shoudn't this be private?
 	void removePoints(); //clears all the points who arent used from the vecPoints
 };
